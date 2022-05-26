7db00180281e5baed5d6e8d0664c677c837114e0
Estevao4Innov <estevao.silva@4innovation.co>
2022-05-25 23:45:27 -0300

    VLU Changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,9 @@
 export default {
-    apiKey: "",
-    authDomain: "watsappclone-d2753.firebaseapp.com",
-     projectId: "watsappclone-d2753",
-    storageBucket: "watsappclone-d2753.appspot.com",
-    messagingSenderId: "904455913846",
-    appId: ""
-}
+  apiKey: "AIzaSyCez5LEQhsfc20E1VSVTGcr4Dp5PXL-u1o",
+  authDomain: "chat-ionic-67ffe.firebaseapp.com",
+  projectId: "chat-ionic-67ffe",
+  storageBucket: "chat-ionic-67ffe.appspot.com",
+  messagingSenderId: "370383193360",
+  appId: "1:370383193360:web:2eba64951f0f3661465195",
+  measurementId: "G-80JM2LMCBB",
+};
